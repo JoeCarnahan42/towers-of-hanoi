2dd64557775b6783634fe4bcd2f0114b9d3b5670
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2024-10-25 07:54:00 -0500

    Begin function for disk movement

--- main.js
+++ main.js
@@ -4,13 +4,15 @@
 //  cannot move larger discs on top of smaller discs
 
 
-
-
+// Tracks Board State
 let gameBoardObj = {
-  peg1: [5, 4, 3, 2, 1],
-  peg2: [],
-  peg3: []
+  1: [5, 4, 3, 2, 1],
+  2: [],
+  3: []
 }
+
+
+
 
 // Handles Opening Display of Game Board
 Object.values(gameBoardObj).map((game) => {
@@ -22,3 +24,22 @@ Object.values(gameBoardObj).map((game) => {
   console.log(gameDisplay)
   
 })
+
+// Handles Movement of Pegs
+let moveDisk = function(peg1, peg2) {
+  const startingPeg = gameBoardObj[peg1]
+  const endingPeg = gameBoardObj[peg2]
+  const diskToMove = Math.min(...startingPeg)
+
+  if (diskToMove < undefined) {
+  // Need to check if disk currently on peg is larger than disc to be moved
+  }
+  console.log(diskToMove)
+  console.log(endingPeg)
+}
+
+moveDisk(1, 2)
+
+
+
+
