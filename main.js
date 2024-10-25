d5177c1b6e7fe9d1e198d2c8d306da7cb99cfba3
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2024-10-25 14:23:04 -0500

    Add all major functions and variables. Win condition check not functional.

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-// Move discs
 // checkWinner function that checks for winning
 //  --> game ends when winner is determined and console logs it
-//  cannot move larger discs on top of smaller discs
 
 
-// Tracks Board State
+// Tracks Board State //
 let gameBoardObj = {
   1: [5, 4, 3, 2, 1],
   2: [],
@@ -14,32 +12,57 @@ let gameBoardObj = {
 
 
 
-// Handles Opening Display of Game Board
-Object.values(gameBoardObj).map((game) => {
+// Handles Opening Display of Game Board //
+let renderGame = function() {Object.values(gameBoardObj).map((game) => {
   const pegs = game.toString()
   let gameString = `--- ${pegs}`
   
   let gameDisplay = gameString.split(",").join(" ")
-
+  
+  
   console.log(gameDisplay)
   
-})
+})}
 
-// Handles Movement of Pegs
-let moveDisk = function(peg1, peg2) {
-  const startingPeg = gameBoardObj[peg1]
-  const endingPeg = gameBoardObj[peg2]
-  const diskToMove = Math.min(...startingPeg)
+// Renders Game Board in Console //
+renderGame()
 
-  if (diskToMove < undefined) {
-  // Need to check if disk currently on peg is larger than disc to be moved
+// Handles the Win Condition and Resets the Game //
+let winState = function() {
+  console.log("Congratulations! You win!")
+  gameBoardObj = {
+    1: [5, 4, 3, 2, 1],
+    2: [],
+    3: []
   }
-  console.log(diskToMove)
-  console.log(endingPeg)
+  renderGame()
 }
 
-moveDisk(1, 2)
+// Handles Movement of Discs //
+let moveDisk = function(startPeg, endPeg) {
+  const startingPeg = gameBoardObj[startPeg]
+  const endingPeg = gameBoardObj[endPeg]
+  const discToMove = startingPeg.at(-1)
+  const endPegDisc = endingPeg.at(endPeg.length)
+  const fullPeg = [5, 4, 3, 2, 1]
+  const emptyPeg = []
 
+  
 
+  if (endPegDisc === undefined || endPegDisc > discToMove) {
+    endingPeg.push(discToMove)
+    startingPeg.pop()
 
+    console.log('Move successful, board is now:')
+    renderGame()
+
+  } else {
+    console.log("Invalid Move: Disks can only be placed on top of smaller disks.")
+    
+  }
+
+  if (gameBoardObj[1].every(emptyPeg) && gameBoardObj[2].every(fullPeg) && gameBoardObj[3].every(emptyPeg)) {
+    winState()
+  }
+}
 
